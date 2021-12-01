4dce3f8a078d7f2f09018e4e50fda074de14ed88
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-12-01 08:16:16 -0500

    day 1 part 2

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -14,9 +14,7 @@ fn part_1() -> io::Result<i32> {
     let mut increases = 0;
 
     for line in reader.lines() {
-        let val = line?
-            .parse::<i32>()
-            .expect("Could not parse line as string");
+        let val = line?.parse::<i32>().unwrap();
 
         if val > prev {
             increases += 1;
@@ -28,12 +26,40 @@ fn part_1() -> io::Result<i32> {
     Ok(increases)
 }
 
-// fn part_2() {
-//     panic!();
-// }
+fn part_2() -> io::Result<i32> {
+    let reader = read_input()?;
+    let mut window = Vec::new();
+    let mut increases = 0;
+
+    for (i, line) in reader.lines().enumerate() {
+        let val = line?.parse::<i32>().unwrap();
+
+        if i < 3 {
+            window.push(val);
+            continue;
+        }
+
+        let mut new_window = window[1..].to_vec();
+        new_window.push(val);
+
+        if new_window.iter().sum::<i32>() > window.iter().sum::<i32>() {
+            increases += 1;
+        }
+
+        window = new_window;
+    }
+
+    Ok(increases)
+}
 
 #[test]
 fn test_part_1() -> std::io::Result<()> {
     println!("Day 1, Part 1: {}", part_1()?);
+    Ok(())
+}
+
+#[test]
+fn test_part_2() -> std::io::Result<()> {
+    println!("Day 1, Part 2: {}", part_2()?);
     Ok(())
 }
