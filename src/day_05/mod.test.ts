22bbdd3e18e6f16e18e89203e54f69dd5d4e9d0a
Mehdi Vasigh <mehdi.vasigh@gmail.com>
2021-12-05 13:54:17 -0500

    code style

--- src/day_05/mod.test.ts
+++ src/day_05/mod.test.ts
@@ -1,4 +1,4 @@
-import * as util from "../util.ts";
+import { format, range } from "../util.ts";
 
 async function readInput(): Promise<string> {
   const txt = await Deno.readTextFile("src/day_05/input.txt");
@@ -14,22 +14,19 @@ function parseInput(input: string): number[][] {
 function overlaps(lines: number[][], diagonals = false): number {
   const pts: { [key: string]: number } = {};
 
-  for (const line of lines) {
-    const [x1, y1, x2, y2] = line;
-
-    if (!diagonals && (y2 !== y1 && x2 !== x1)) continue;
+  for (const [x1, y1, x2, y2] of lines) {
+    if (!diagonals && y2 !== y1 && x2 !== x1) continue;
 
     const dx = x2 - x1;
     const dy = y2 - y1;
     const end = Math.max(Math.abs(dx), Math.abs(dy));
 
-    for (let i = 0; i <= end; i++) {
+    for (let i of range(end + 1)) {
       const x = x1 + Math.round((i / end) * dx);
       const y = y1 + Math.round((i / end) * dy);
       const key = `${x},${y}`;
 
-      pts[key] ??= 0;
-      pts[key] += 1;
+      (pts[key] ??= 0) != null && (pts[key] += 1);
     }
   }
 
@@ -41,17 +38,17 @@ function part1(lines: number[][]): number {
 }
 
 function part2(lines: number[][]): number {
-  return overlaps(lines, true)
+  return overlaps(lines, true);
 }
 
 Deno.test("Day 5, Part 1:", async () => {
   const input = await readInput();
   const lines = parseInput(input);
-  console.log(util.format(part1(lines)));
+  console.log(format(part1(lines)));
 });
 
 Deno.test("Day 5, Part 2:", async () => {
   const input = await readInput();
   const lines = parseInput(input);
-  console.log(util.format(part2(lines)));
+  console.log(format(part2(lines)));
 });
--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,6 @@ import * as Colors from "https://deno.land/std@0.116.0/fmt/colors.ts";
 
 export const format = (val: number): string =>
   Colors.bgGreen(Colors.black(" " + val.toString() + " "));
+
+export const range = (length: number): number[] =>
+  Array.from({ length }, (_, i) => i);
